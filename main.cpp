7c5e03f8168848b82782b1a6ac87b8719f018c0e
Aleksandr-Popov-22 <43619329+Aleksandr-Popov-22@users.noreply.github.com>
2018-10-04 11:42:58 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main()//task 3
+{
+      int a; int b; int c;
+      std::cout << "a= ";
+      std::cin >> a;
+      std::cout << "b= ";
+      std:: cin >> b;
+      std::cout << "c= ";
+      std::cin >> c;
+      std::cout << a << "  " << b << "  " << c << std::endl;
+      return 0;
+
+}
